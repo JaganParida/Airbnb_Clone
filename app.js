b8f2916baded8b29dfc4c5f3df37222291c031c9
Jagan Parida <jaganparida39064@gmail.com>
2025-08-01 23:53:31 +0530

    Add delete route

--- app.js
+++ app.js
@@ -65,6 +65,14 @@ app.put("/listings/:id", async (req, res) => {
   res.redirect(`/listings/${id}`);
 });
 
+//delete
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
+});
+
 //server listening
 app.listen(8080, () => {
   console.log("server is listening to port 8080");
