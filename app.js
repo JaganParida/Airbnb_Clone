5fec03d2fb4c0c1e1fe20128bc33b83b64446065
Jagan Parida <jaganparida39064@gmail.com>
2025-08-01 20:45:34 +0530

    Listing models

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
 
 //database connection
-const dbUrl = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = "mongodb://127.0.0.1:27017/WanderLodge";
 
 main()
   .then(() => {
@@ -16,6 +17,26 @@ main()
 async function main() {
   await mongoose.connect(dbUrl);
 }
+
+app.get("/testListing", async (req, res) => {
+  let sampleListing = new Listing({
+    title: "My New Villa",
+    description: "By the beach",
+    price: 1200,
+    location: "Calangute, Goa",
+    country: "India",
+    image: "",
+  });
+
+  try {
+    await sampleListing.save();
+    console.log("Sample was saved");
+    res.send("Successful testing");
+  } catch (err) {
+    console.error("Error saving sample:", err);
+    res.status(500).send("Error occurred");
+  }
+});
 
 //server listening
 app.listen(8080, () => {
---
+++ models/listing.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    type: String,
+    set: (v) =>
+      v === ""
+        ? "https://images.unsplash.com/photo-1741851373505-c07b8a723560?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyMHx8fGVufDB8fHx8fA%3D%3D"
+        : v,
+  },
+  price: Number,
+  location: String,
+  country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
