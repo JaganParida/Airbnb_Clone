5c038cb7aca70471ce5041ea686c9192ede83271
Jagan Parida <jaganparida39064@gmail.com>
2025-08-04 23:36:51 +0530

    Add public folder

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 //index
 app.get("/listings", async (req, res) => {
