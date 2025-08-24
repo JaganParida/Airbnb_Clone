11ce637e26b8d6c064eb4cf3531ddd9a622b7640
Jagan Parida <jaganparida39064@gmail.com>
2025-08-24 23:22:41 +0530

    Add signup.ejs

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/signup", (req, res) => {
-  res.send("form");
+  res.render("users/signup.ejs");
 });
 module.exports = router;
