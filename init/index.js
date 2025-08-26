9551ccab34e45dc5d9eedde1e5e2c568e310b17b
Jagan Parida <jaganparida39064@gmail.com>
2025-08-26 21:06:45 +0530

    update in index.js

--- init/index.js
+++ init/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const dbUrl = "mongodb://127.0.0.1:27017/WanderLodge";
+const MONGO_URL = "mongodb://127.0.0.1:27017/WanderLodge";
 
 main()
   .then(() => {
@@ -13,13 +13,17 @@ main()
   });
 
 async function main() {
-  await mongoose.connect(dbUrl);
+  await mongoose.connect(MONGO_URL);
 }
 
 const initDB = async () => {
   await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "6785511723eb6bcfc840e6e9",
+  }));
   await Listing.insertMany(initData.data);
-  console.log("data was initializd");
+  console.log("data was initialized");
 };
 
 initDB();
