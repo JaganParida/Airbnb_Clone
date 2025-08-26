b0c152bf2bd494ef1bf9fa41fa1cf2838a1ce9ad
Jagan Parida <jaganparida39064@gmail.com>
2025-08-26 20:41:23 +0530

    Update in user.js for automatic login

--- routes/user.js
+++ routes/user.js
@@ -15,8 +15,11 @@ router.post(
       const newUser = new User({ email, username });
       const registeredUser = await User.register(newUser, password);
       console.log(registeredUser);
-      req.flash("success", "Welcome to Wanderlust!");
-      res.redirect("/listings");
+      req.login(registeredUser, (err) => {
+        if (err) {
+          return next(err);
+        }
+      });
     } catch (e) {
       req.flash("error", e.message);
       res.redirect("/signup");
