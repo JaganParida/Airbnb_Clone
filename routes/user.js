b68544d3a9ec994dcf8d2970ea4c63cd68b6d0f6
Jagan Parida <jaganparida39064@gmail.com>
2025-08-26 20:48:22 +0530

    Update in user.js

--- routes/user.js
+++ routes/user.js
@@ -1,56 +1,31 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user");
 const wrapAsync = require("../utils/wrapAsync.js");
+const passport = require("passport");
+const { saveRedirectUrl } = require("../middleware.js");
 
-router.get("/signup", (req, res) => {
-  res.render("users/signup.ejs");
-});
+const userController = require("../controllers/users.js");
 
-router.post(
-  "/signup",
-  wrapAsync(async (req, res) => {
-    try {
-      let { username, email, password } = req.body;
-      const newUser = new User({ email, username });
-      const registeredUser = await User.register(newUser, password);
-      console.log(registeredUser);
-      req.login(registeredUser, (err) => {
-        if (err) {
-          return next(err);
-        }
-      });
-    } catch (e) {
-      req.flash("error", e.message);
-      res.redirect("/signup");
-    }
-  })
-);
+//signup route
+router
+  .route("/signup")
+  .get(userController.renderSignupForm)
+  .post(wrapAsync(userController.signup));
 
-router.get("/login", (req, res) => {
-  res.render("user/login.ejs");
-});
+//login route
+router
+  .route("/login")
+  .get(userController.renderLoginForm)
+  .post(
+    saveRedirectUrl,
+    passport.authenticate("local", {
+      failureRedirect: "/login",
+      failureFlash: true,
+    }),
+    userController.login
+  );
 
-router.post(
-  "/login",
-  passport.authenticate("local", {
-    failureRedirect: "/login",
-    failureFlash: true,
-  }),
-  async (req, res) => {
-    res.send("success", "Welcome to Wanderlust! You are logged in!");
-    res, redirect("/listings");
-  }
-);
+//logout route
+router.get("/logout", userController.logout);
 
-router.get("/logout", (req, res) => {
-  req.logOut((err) => {
-    if (err) {
-      return nnext();
-    }
-    req.flash("you are logout now");
-    res.redirect("/listings");
-  });
-});
-
-reportError.module.exports = router;
+module.exports = router;
