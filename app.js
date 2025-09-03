51898b7654261af840e068997fdee111d07d5874
Jagan Parida <jaganparida39064@gmail.com>
2025-09-03 17:45:13 +0530

    Small change in style of listings

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
-//routes in listings
+//routes
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
